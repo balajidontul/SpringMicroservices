cfe3897da5b16dcc1217a24e3fa0179cd4628322
balajidontul <balajidontulofficial@outlook.com>
2023-06-08 23:03:11 +0530

    Added config server

---
+++ ConfigServer/src/test/java/com/edu/ConfigServer/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.ConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
