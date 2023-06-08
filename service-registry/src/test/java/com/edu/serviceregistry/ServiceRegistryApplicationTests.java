dbb155bf74df8c5b2e898f50bf84069b07729b35
balajidontul <balajidontulofficial@outlook.com>
2023-06-08 20:50:42 +0530

    Added service registry

---
+++ service-registry/src/test/java/com/edu/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
